59d64bac0a9b58b71a2e142df893aeca0c8796fc
kc1212 <kc1212@users.noreply.github.com>
2016-10-27 12:29:15 +0200

    Revert "Move master and slave to lib"
    This reverts commit 2126db6649b4671ea3e8baaf3aae4b0f6e4a6b6c.

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/aaronang/cong-the-ripper/lib/master"
+import "github.com/aaronang/cong-the-ripper/master"
 
 func main() {
 	fmt.Println("master starting...")
