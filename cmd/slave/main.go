c447a86b2209052fb8b71cfc4555097d6f595f40
Victor Li <victor@victorli.nl>
2016-10-28 20:55:34 +0200

    Remove Ptr from the variable names

--- cmd/slave/main.go
+++ cmd/slave/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"log"
 
-	flag "github.com/ogier/pflag"
-
 	"github.com/aaronang/cong-the-ripper/lib/slave"
+	flag "github.com/ogier/pflag"
 )
 
 func main() {
 	log.Println("slave starting...")
-	portPtr := flag.String("port", "8080", "Web server port")
-	masterIpPtr := flag.String("master-ip", "localhost", "Ip address of the master")
-	masterPortPtr := flag.String("master-port", "8080", "Port of the master")
+	port := flag.String("port", "8080", "Web server port")
+	masterIP := flag.String("master-ip", "localhost", "Ip address of the master")
+	masterPort := flag.String("master-port", "8080", "Port of the master")
 	flag.Parse()
 
-	s := slave.Init("instance.EC2.cong1", *portPtr, *masterIpPtr, *masterPortPtr)
+	s := slave.Init("instance.EC2.cong1", *port, *masterIP, *masterPort)
 	s.Run()
 }
