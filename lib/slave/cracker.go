ed31ee89920ff73351ba940357d7ebd6470dccf0
kc1212 <kc1212@users.noreply.github.com>
2016-11-06 14:32:10 +0100

    Alternative progress update logic

--- lib/slave/cracker.go
+++ lib/slave/cracker.go
@@ -13,25 +13,26 @@ func Execute(task *task, successChan chan CrackerSuccess, failChan chan CrackerF
 	var hasher hasher.Hasher = new(hasher.Pbkdf2) // Can be swapped with other hashing algorithms
 
 	log.Println("[ Task", task.ID, "]", "Start cracker.Execute")
+outer:
 	for {
-		if candidate := bd.Next(); candidate != nil {
-			select {
-			case c := <-task.progressChan:
-				// Return reversed array to match reversed encoding in the master
-				c <- lib.ReverseArray(candidate)
-			default:
+		var candidate []byte
+		select {
+		case c := <-task.progressChan:
+			// Return reversed array to match reversed encoding in the master
+			c <- lib.ReverseArray(candidate)
+		default:
+			if candidate = bd.Next(); candidate != nil {
+				hash := hasher.Hash(candidate, &task.Task)
+				// fmt.Println("Key base64: " + string(candidate) + " -> " + b64.StdEncoding.EncodeToString(hash))
+				if lib.TestEqBytes(hash, task.Digest) {
+					successChan <- CrackerSuccess{taskID: task.ID, password: string(candidate)}
+					break outer
+				}
+			} else {
+				failChan <- CrackerFail{taskID: task.ID}
+				bd.Close()
+				break outer
 			}
-
-			hash := hasher.Hash(candidate, &task.Task)
-			// fmt.Println("Key base64: " + string(candidate) + " -> " + b64.StdEncoding.EncodeToString(hash))
-			if lib.TestEqBytes(hash, task.Digest) {
-				successChan <- CrackerSuccess{taskID: task.ID, password: string(candidate)}
-				break
-			}
-		} else {
-			failChan <- CrackerFail{taskID: task.ID}
-			bd.Close()
-			break
 		}
 	}
 	log.Println("[ Task", task.ID, "]", "Finished cracker.Execute")
